dae1b690964ae7636fb9e8b4415bcf39807a5353
IsaacBianchini <159019994+IsaacBianchini@users.noreply.github.com>
2024-04-06 14:55:19 -0300

    Update apresentacaoScript.js

--- apresentacaoScript.js
+++ apresentacaoScript.js
@@ -1,11 +1,27 @@
-function enviarEmail() {
-    Email.send({
-      Host: "smtp.elasticemail.com",
-      Username: "isaacbr871@gmail.com",
-      Password: "970917EEA271AD3158B60C1A0EBC8FE6EAAA",
-      To: "emprogresso4@gmail.com",
-      From: "isaacbr871@gmail.com",
-      Subject: "This is the subject",
-      Body: "And this is the body",
-    }).then((message) => alert(message));
-  }
+document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (grecaptcha.getResponse() === "") {
+      document.getElementById("captcha-error").style.display = "block";
+    } else {
+      document.getElementById("captcha-error").style.display = "none";
+      EnviarEmail();
+    }
+  });
+
+function EnviarEmail() {
+  let nome = document.getElementById("inputNome").value;
+  let email = document.getElementById("inputEmail").value;
+  let negocio = document.getElementById("inputNegocio").value;
+  let descricao = document.getElementById("textDescricao").value;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "isaacbr871@gmail.com",
+    Password: "970917EEA271AD3158B60C1A0EBC8FE6EAAA",
+    To: "isaacbr871@gmail.com",
+    From: "isaacbr871@gmail.com",
+    Subject: "Formulário Lkp-Films (Entre-em-Contato)",
+    Body: `Nome: ${nome} <br> Email: ${email} <br> Negócio: ${negocio} <br> Descrição: ${descricao}`,
+  }).then((message) => alert(message));
+}
